3551944eb74639e7534bb2eecc7b579338b9e808
Adrielle <adriellesantosalves@hotmail.com>
2021-09-22 22:35:03 -0300

    feat: create form for LoginCreate component

--- src/components/LoginCreate.vue
+++ src/components/LoginCreate.vue
@@ -1,13 +1,46 @@
 <template>
-  <p>login criar</p>
+  <section>
+    <h2>Create account</h2>
+    <transition mode="out-in">
+      <button v-if="!create" class="btn" @click="create = true">Create Account</button>
+      <UserForm v-else>
+        <button class="btn btn-form">Create account</button>
+      </UserForm>
+    </transition>
+  </section>
 </template>
 
 <script>
+import UserForm from '@/components/UserForm.vue';
+
 export default {
   name: 'LoginCreate',
+  components: {
+    UserForm,
+  },
+  data() {
+    return {
+      create: false,
+    };
+  },
 };
 </script>
 
 <style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
 
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
 </style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form>
+    <label for="name">Name</label>
+    <input id="name" name="name" type="text" v-model="name">
+
+    <label for="username">Username</label>
+    <input id="username" name="username" type="text" v-model="username">
+
+    <label for="email">Email</label>
+    <input id="email" name="email" type="email" v-model="email">
+
+    <label for="password">Password</label>
+    <input id="password" name="password" type="password" v-model="password">
+
+    <label for="city">City</label>
+    <input id="city" name="city" type="text" v-model="city">
+
+    <label for="street">Street</label>
+    <input id="street" name="street" type="text" v-model="street">
+
+    <label for="number">Number</label>
+    <input id="number" name="number" type="text" v-model="number">
+
+    <label for="zipcode">Zipcode</label>
+    <input id="zipcode" name="zipcode" type="text" v-model="zipcode">
+
+    <label for="phone">Phone</label>
+    <input id="phone" name="phone" type="text" v-model="phone">
+
+    <div class="button">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  align-items: center;
+  display: grid;
+  grid-template-columns: 80px 1fr;
+}
+
+.button {
+  grid-column: 2;
+  margin-top: 10px;
+}
+</style>
